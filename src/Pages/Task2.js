6cc1fde44bc6add90c0c3ce6cdf26c309646a5e3
Khushi Arora <akhushi72@gmail.com>
2023-07-10 05:14:25 +0530

    task2 done

--- src/Pages/Task2.js
+++ src/Pages/Task2.js
@@ -1,7 +1,26 @@
+import {useState} from 'react';
 import MotionHoc from "./MotionHoc";
 
 const Task2Component = () => {
-  return <h1>Task2</h1>;
+  let [count, changeCount] = useState(0);
+  return (
+    <>
+    <h1>Task #2</h1>
+    <h1>Using React Hooks</h1>
+		<div class="counter">
+			<h2>{count}</h2>
+			<button onClick={()=>changeCount(count - 1)}>
+				<span>-1</span>
+			</button>
+			<button onClick={()=>changeCount(count + 1)}>
+				<span>+1</span>
+			</button>
+			<button onClick={()=>changeCount(count = 0)}>
+				<span>Reset</span>
+			</button>
+		</div>
+    </>
+	)
 };
 
 const Task2 = MotionHoc(Task2Component);
